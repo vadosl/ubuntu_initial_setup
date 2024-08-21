2423f809c62a154127365cf24bd850adb0c3b94c
vadosl <vlatypov.spb@gmail.com>
2024-08-21 16:04:59 +0300

    fix mistakes

--- initial_setup.sh
+++ initial_setup.sh
@@ -246,7 +246,7 @@ check
 
 run "Настройка конфига "
   
-  execAsUser ${username} 'cp ~/ubuntu_initial_setup/configs/vim/.* ~'
+  execAsUser ${username} 'cp ~/ubuntu_initial_setup/configs/vim/. ~/'
   
 check
 
